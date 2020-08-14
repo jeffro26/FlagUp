96f165e8b9bed290703350d56573c3253991eb19
9503663 <a9503663@9503663s-mbp.home>
2020-08-14 12:31:26 +0100

    Added toggle theme functionality

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,36 +1,71 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import PublicIcon from '@material-ui/icons/Public';
+import React from "react";
+import { makeStyles , withStyles} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import PublicIcon from "@material-ui/icons/Public";
+import Switch from "@material-ui/core/Switch";
+import Tooltip from '@material-ui/core/Tooltip';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
+  },
+  appBar: {
+    background: props => (props.appTheme === "LT" ? "#1a237e" : "#212121")
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
-export default function FlagAppBar() {
-  const classes = useStyles();
+const ThemeTooltip = withStyles({
+  tooltip: {
+    color: "black",
+    backgroundColor: "white",
+    fontSize: 21
+  }
+})(Tooltip);
+
+export default function FlagAppBar(props) {
+  const classes = useStyles(props);
+  const [state, setState] = React.useState({
+    themeSwitch: true
+  });
+
+  const handleChange = event => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+    props.setTheme();
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <PublicIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Welcome to the world
           </Typography>
+          <ThemeTooltip title={"Theme Toggle"} arrow>
+          <Switch
+            checked={state.checkedB}
+            onChange={handleChange}
+            color="primary"
+            name="themeSwitch"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+          </ThemeTooltip>
         </Toolbar>
       </AppBar>
     </div>
